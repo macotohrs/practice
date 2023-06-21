6fac1239919b42630050acf51844533b9c0876ee
macotonacamura <65491593+macotonacamura@users.noreply.github.com>
2023-06-21 18:17:29 +0900

    pokemonコンポーネントの実装

---
+++ src/app/pokemon/page.tsx
@@ -0,0 +1,109 @@
+"use client";
+import { FC } from "react";
+import { Suspense } from "react";
+import { atom, Provider, useAtom } from "jotai";
+import Image from "next/image";
+
+interface BaseStats {
+  HP: number;
+  Attack: number;
+  Defense: number;
+  "Sp. Attack": number;
+  "Sp. Defense": number;
+  Speed: number;
+}
+
+interface PokemonName {
+  english: string;
+  japanese: string;
+  chinese: string;
+  french: string;
+}
+
+interface IPokemon {
+  id: number;
+  name: PokemonName;
+  type: string[];
+  base: BaseStats;
+}
+
+const URL =
+  "https://gist.githubusercontent.com/jherr/23ae3f96cf5ac341c98cd9aa164d2fe3/raw/f8d792f5b2cf97eaaf9f0c2119918f333e348823/pokemon.json";
+
+const filterAtom = atom("");
+
+export const pokemonAtom = atom(async () => {
+  const resp = await fetch(URL);
+  const data = await resp.json();
+  return data as IPokemon[];
+});
+
+export const filteredPokemonAtom = atom(async (get) =>
+  (await get(pokemonAtom)).filter((p) =>
+    p.name.english.toLowerCase().includes(get(filterAtom).toLowerCase())
+  )
+);
+
+const FilterInput: FC = () => {
+  const [filter, setFilter] = useAtom(filterAtom);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFilter(e.target.value);
+  };
+
+  return (
+    <input
+      className="text-cyan-700 mr-9 p-2 w-full rounded-md mb-10"
+      value={filter}
+      onChange={handleChange}
+    />
+  );
+};
+
+const PokemonTable: FC = () => {
+  const [pokemon] = useAtom(filteredPokemonAtom);
+
+  return (
+    <table className="w-full bg-black text-white">
+      <tbody className="h-40 overflow-y-auto">
+        {pokemon.map((p, index) => (
+          <tr key={index}>
+            <td className="p-5">{p.id}</td>
+            <td className="p-5">{p.name.english}</td>
+            <td className="p-5">{p.type.join(", ")}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+const PokemonList: FC = () => {
+  return (
+    <div className="flex">
+      <div className="App mr-20">
+        <h1 className="text-3xl mb-7">Capture Pokemon</h1>
+        <FilterInput />
+        <PokemonTable />
+      </div>
+      <div className="image-container">
+        <Image
+          src="/bug_mushitori_boy.png"
+          alt="Plate"
+          width={300}
+          height={100}
+        />
+      </div>
+    </div>
+  );
+};
+
+const Pokemon: FC = () => (
+  <Provider>
+    <Suspense fallback={<div>Loading...</div>}>
+      <PokemonList />
+    </Suspense>
+  </Provider>
+);
+
+export default Pokemon;
