a3ce06dbdb365ea1fd0b5d14fa33970841215f1e
macotonacamura <65491593+macotonacamura@users.noreply.github.com>
2023-06-22 13:12:04 +0900

    fetch処理を試す

--- src/app/users/UserList.tsx
+++ src/app/users/UserList.tsx
@@ -6,12 +6,17 @@ type User = {
 
 const UserList = async () => {
   await new Promise((resolve) => setTimeout(resolve, 5000));
-  const response = await fetch('http://localhost:3000/api'); // Route handlerを経由してデータの取得
+  // const response = await fetch('http://localhost:3000/api'); // Route handlerを経由してデータの取得
   // const response = await fetch("https://jsonplaceholder.typicode.com/users"); // jsonplaceholderから直にfetch
   // const response = await fetch('https://jsonplaceholder.typicode.com/userXXXX'); // エラーメッセージを出現させるためのダミー
-  if (!response.ok) throw new Error('Failed to fetch data');
+
+  // api/route.tsに記述した console.log(name)が反応する
+  const response = await fetch("http://localhost:3000/api?name=John", {
+    // cache: 'no-store',
+  });
+
+  if (!response.ok) throw new Error("Failed to fetch data");
   const users: User[] = await response.json();
-  console.log("💚", users)
   return (
     <ul>
       {users.map((user, index) => (
