d63deadcaaf3281d6587b6f87bca1753ac8f9963
macotonacamura <65491593+macotonacamura@users.noreply.github.com>
2023-06-22 13:13:31 +0900

    fetch処理を試す

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -5,6 +5,19 @@ import Image from "next/image";
 
 const Page = async () => {
   // Suspenseを使用する場合は、Suspenseで囲った内容がloadingされる(以下の場合<h1>は表示される)
+  const response = await fetch('http://localhost:3000/api', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      name: 'John',
+      email: 'john@example.com',
+    }),
+  });
+
+  const data = await response.json();
+  console.log("data", data)
   return (
     <div className="m-4">
       <h1 className="text-lg font-bold">User List</h1>
@@ -15,13 +28,13 @@ const Page = async () => {
         <Suspense
           fallback={
             <div className="flex justify-center items-center h-screen font-bold">
-              {/* <Image
+              <Image
               src="/mark_arrow_reload.png"
               alt="loading"
               width={180}
               height={180}
               className="rotating-image"
-            /> */}
+            />
               <div className="ml-2">Loading...</div>
             </div>
           }
