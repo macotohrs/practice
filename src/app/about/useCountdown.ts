9a31981bf5d8503c10dacb8c5da4cb5778f774aa
macotonacamura <65491593+macotonacamura@users.noreply.github.com>
2023-06-22 11:33:34 +0900

    about/派生アトムでstate更新してみる

--- src/app/about/useCountdown.ts
+++ src/app/about/useCountdown.ts
@@ -1,36 +1,38 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { useRouter } from "next/navigation";
-// import { useAtom, atom } from "jotai";
+import { useAtom } from "jotai";
+import {
+  clicked,
+  addingCountAtom
+} from "../../jotai/atoms";
 
 const useCountdown = () => {
-  const [count, setCount] = useState(0);
-  const [isClicked, setClicked] = useState(false);
+  const [isClicked, setClicked] = useAtom(clicked);
+  const [count, add] = useAtom(addingCountAtom);
   const router = useRouter();
 
   useEffect(() => {
     let countdownInterval: NodeJS.Timeout;
-
     if (isClicked && count > 0) {
       countdownInterval = setInterval(() => {
-        setCount((prevCount) => prevCount - 1);
+        add(-1); // countを1ずつ減らす
       }, 1000);
     } else if (isClicked && count === 0) {
       countdownInterval = setTimeout(() => {
         router.push("/about/info");
-      }, 5000);
+      }, 4000);
     }
-
     return () => {
       clearInterval(countdownInterval);
     };
-  }, [isClicked, count, router]);
+  }, [isClicked, count, router, add]);
 
   const handleClick = () => {
     setClicked(true);
-    setCount(5);
+    add(5);
   };
 
-  return { count, isClicked, handleClick };
+  return { count , isClicked, handleClick };
 };
 
 export default useCountdown;
--- src/jotai/atoms.tsx
+++ src/jotai/atoms.tsx
@@ -4,3 +4,14 @@ export const counterAtom = atom(0);
 export const testIncrement = atom(0);
 export const testCountedPlates = atom<number[]>([]);
 export const isMany = atom(false);
+export const clicked = atom(false);
+
+// 派生アトムを作成してみる
+export const addingCountAtom = atom(
+  (get) => get(counterAtom), // read関数ではそのままcountAtomの値を返す
+  (get, set, num:number) => { // write関数では与えられた値を加算する
+    // num : 呼び出し元の関数の引数に入れた数字が渡ってくる
+    set(counterAtom, get(counterAtom) + num)
+  }
+)
+
