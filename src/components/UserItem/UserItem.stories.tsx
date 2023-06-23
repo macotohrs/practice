582534398de020dce382698cd1b7d10a811bbd1a
macotonacamura <65491593+macotonacamura@users.noreply.github.com>
2023-06-23 13:56:32 +0900

    componentにUserItemコンポーネントを作成  Loadersを利用してデータを取得

---
+++ src/components/UserItem/UserItem.stories.tsx
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import UserItem from "./UserItem";
+
+const meta = {
+  title: "UserItem",
+  component: UserItem,
+  tags: ["autodocs"],
+} satisfies Meta<typeof UserItem>;
+
+export default meta;
+
+type Story = StoryObj<typeof UserItem>;
+// 外部サービスから非同期に取得したデータを Loaders によって利用することができる
+export const Default: Story = {
+  args: {
+    id: "1",
+    name: "John Doe",
+  },
+};
+
+export const FetchData: Story = {
+  loaders: [
+    async () => ({
+      user: await (
+        await fetch('https://jsonplaceholder.typicode.com/users/1')
+      ).json(),
+    }),
+  ],
+  render: (args, { loaded: { user } }) => (
+    <UserItem {...args} id={user.id} name={user.name} />
+  ),
+};
