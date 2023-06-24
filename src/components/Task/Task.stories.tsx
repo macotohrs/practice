16b4b1baf45cfcc556cb1445673683ff0a64bdb3
macotonacamura <65491593+macotonacamura@users.noreply.github.com>
2023-06-24 17:17:21 +0900

    Task.tsxをアップデート

--- src/components/Task/Task.stories.tsx
+++ src/components/Task/Task.stories.tsx
@@ -9,59 +9,108 @@ export default {
     onEditTitle: { action: "onEditTitle" },
   },
   parameters: {
-    backgrounds: { // canvas上の背景色の設定はstory単位でできる
+    backgrounds: {
+      // canvas上の背景色の設定はstory単位でできる
       values: [
-        { name: 'Navy', value: '#06256d' },
-        { name: 'Gray', value: '#393939' },
+        { name: "Navy", value: "#06256d" },
+        { name: "Gray", value: "#393939" },
       ],
     },
   },
 };
 
 export enum State {
-  inbox = 'TASK_INBOX',
-  pinned = 'TASK_PINNED',
-  archived = 'TASK_ARCHIVED',
+  inbox = "TASK_INBOX",
+  pinned = "TASK_PINNED",
+  archived = "TASK_ARCHIVED",
 }
 
 export const Default = {
   args: {
-    task: {
-      id: "1",
-      title: "Buy milk",
-      state: State.inbox,
-    },
+    tasks: [
+      { id: "1", state: State.inbox, title: "Build a date picker" },
+      { id: "2", state: State.inbox, title: "QA dropdown" },
+      {
+        id: "3",
+        state: State.inbox,
+        title: "Write a schema for account avatar component",
+      },
+      { id: "4", state: State.inbox, title: "Export logo" },
+      { id: "5", state: State.inbox, title: "Fix bug in input error state" },
+      {
+        id: "6",
+        state: State.inbox,
+        title: "Draft monthly blog to customers",
+      },
+    ],
+  },
+};
+
+export const WithPinnedTasks = {
+  args: {
+    tasks: [
+      {
+        id: "6",
+        title: "Draft monthly blog to customers",
+        state: "TASK_PINNED",
+      },
+      ...Default.args.tasks.slice(0, 5),
+    ],
   },
 };
 
 export const Pinned = {
   args: {
-    task: {
-      id: "2",
-      title: "QA dropdown",
-      state: State.pinned
-    },
+    tasks: [
+      {
+        id: "8",
+        title: "QA dropdown",
+        state: State.pinned,
+      },
+    ],
   },
 };
-
+console.log("Pinned",Pinned)
+// Pinned 
 export const Archived = {
   args: {
-    task: {
-      id: "3",
-      title: "Write schema for account menu",
-      state: State.archived
-    },
+    tasks: [
+      {
+        id: "9",
+        title: "Write schema for account menu",
+        state: State.archived,
+      },
+    ],
   },
 };
 
-const longTitleString = `This task's name is absurdly large. In fact, I think if I keep going I might end up with content overflow. What will happen? The star that represents a pinned task could have text overlapping. The text could cut-off abruptly when it reaches the star. I hope not!`;
+const longTitleString = `This task"s name is absurdly large. In fact, I think if I keep going I might end up with content overflow. What will happen? The star that represents a pinned task could have text overlapping. The text could cut-off abruptly when it reaches the star. I hope not!`;
 
 export const LongTitle = {
   args: {
-    task: {
-      id: '4',
-      title: longTitleString,
-      state: State.inbox
-    },
+    tasks: [
+      {
+        id: "10",
+        title: longTitleString,
+        state: State.inbox,
+      },
+    ],
   },
 };
+
+export const Loading = {
+  args: {
+    tasks: [],
+    loading: true,
+    empty: false,
+  },
+};
+
+export const Empty = {
+  args: {
+    ...Loading.args,
+    loading: false,
+    empty: true,
+  },
+};
+
--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -3,6 +3,7 @@ import { State } from "./Task.stories";
 import "./Task.css";
 import { FaTrash } from "react-icons/fa";
 import { AiFillStar } from "react-icons/ai";
+import { AiOutlineCheck } from 'react-icons/ai';
 
 interface TaskProps {
   task: {
@@ -14,6 +15,8 @@ interface TaskProps {
   onTogglePinTask: (state: string, id: string) => void;
   onEditTitle: (value: string, id: string) => void;
   onDeleteTask: (id: string) => void;
+  loading?: boolean;
+  empty?: boolean;
 }
 
 const Task: React.FC<TaskProps> = ({
@@ -23,70 +26,123 @@ const Task: React.FC<TaskProps> = ({
   onEditTitle,
   onDeleteTask,
 }) => {
-  return  (
+  return (
     <div
       className={`list-item border border-gray-300 p-4 bg-white ${state} flex items-center justify-between`}
       role="listitem"
       aria-label={`task-${id}`}
     >
-    <div className="flex items-center">
-      <label
-        htmlFor="checked"
-        aria-label={`archiveTask-${id}`}
-        className="checkbox mr-2"
-      >
-        <input
-          type="checkbox"
-          disabled={true}
-          name="checked"
-          id={`archiveTask-${id}`}
-          checked={state === State.archived}
-        />
-        <span
-          className="checkbox-custom"
-          onClick={() => onArchiveTask(State.archived, id)}
-          role="button"
-          aria-label={`archiveButton-${id}`}
-        />
-      </label>
-
-      <label htmlFor="title" aria-label={title} className="title">
-        <input
-          type="text"
-          value={title}
-          name="title"
-          placeholder="Input title"
-          style={{ textOverflow: "ellipsis" }}
-          onChange={(e) => onEditTitle(e.target.value, id)}
-        />
-      </label>
-      <button
-        aria-label="delete"
-        className="delete-button ml-auto"
-        onClick={() => onDeleteTask(id)}
-      >
-        <FaTrash className="text-gray-400" />
-      </button>
-      {state !== State.archived && (
-        <button
-          className="pin-button"
-          onClick={() => onTogglePinTask(state, id)}
-          id={`pinTask-${id}`}
-          aria-label={state === State.pinned ? "unpin" : "pin"}
-          key={`pinTask-${id}`}
+      <div className="flex items-center">
+        <label
+          htmlFor="checked"
+          aria-label={`archiveTask-${id}`}
+          className="checkbox mr-2"
         >
+          <input
+            type="checkbox"
+            disabled={true}
+            name="checked"
+            id={`archiveTask-${id}`}
+            checked={state === State.archived}
+          />
           <span
-            className={
-              state === State.pinned ? `text-orange-300` : `text-slate-400`
-            }
-          >
-            <AiFillStar />
-          </span>
+            className="checkbox-custom"
+            onClick={() => onArchiveTask(State.archived, id)}
+            role="button"
+            aria-label={`archiveButton-${id}`}
+          />
+        </label>
+        <label htmlFor="title" aria-label={title} className="title">
+          <input
+            type="text"
+            value={title}
+            name="title"
+            placeholder="Input title"
+            style={{ textOverflow: "ellipsis" }}
+            onChange={(e) => onEditTitle(e.target.value, id)}
+          />
+        </label>
+        <button
+          aria-label="delete"
+          className="delete-button ml-auto"
+          onClick={() => onDeleteTask(id)}
+        >
+          <FaTrash className="text-gray-400" />
         </button>
-      )}
-    </div>
+        {state !== State.archived && (
+          <button
+            className="pin-button"
+            onClick={() => onTogglePinTask(state, id)}
+            id={`pinTask-${id}`}
+            aria-label={state === State.pinned ? "unpin" : "pin"}
+            key={`pinTask-${id}`}
+          >
+            <span
+              className={
+                state === State.pinned ? `text-orange-300` : `text-slate-400`
+              }
+            >
+              <AiFillStar />
+            </span>
+          </button>
+        )}
+      </div>
     </div>
   );
 };
 
-export default Task;
+interface TasksProps {
+  tasks: {
+    id: string;
+    title: string;
+    state: string;
+  }[];
+  onArchiveTask: (action: string, id: string) => void;
+  onTogglePinTask: (state: string, id: string) => void;
+  onEditTitle: (value: string, id: string) => void;
+  onDeleteTask: (id: string) => void;
+  loading: boolean;
+  empty: boolean;
+}
+
+const Tasks: React.FC<TasksProps> = ({
+  tasks,
+  onArchiveTask,
+  onTogglePinTask,
+  onEditTitle,
+  onDeleteTask,
+  loading,
+  empty,
+}) => {
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen font-bold">
+        <div className="animate-jump text-4xl">Loading...</div>
+      </div>
+    );
+  }
+
+  if (empty) {
+    return (
+      <div className="flex justify-center items-center h-screen font-bold">
+        <AiOutlineCheck className="text-5xl text-green-400 mr-1"/>
+        You have No task!
+      </div>
+    );
+  }
+console.log("😄",tasks)
+  return tasks.map((task) => (
+    <Task
+      key={task.id}
+      task={task}
+      onArchiveTask={onArchiveTask}
+      onTogglePinTask={onTogglePinTask}
+      onEditTitle={onEditTitle}
+      onDeleteTask={onDeleteTask}
+      loading={false}
+    />
+  ));
+};
+
+export default Tasks;
