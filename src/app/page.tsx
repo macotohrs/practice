73b57be2505016372a57c7cce2ed96b5916dd01d
macotonacamura <65491593+macotonacamura@users.noreply.github.com>
2023-06-29 14:56:00 +0900

    cssを修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,26 +12,29 @@ function App() {
       <TrafficLight />
       <Image src="/catonthePC.png" alt="catonthePC" width={300} height={100} />
       <div className="flex justify-center items-center space-x-4 mt-4">
-        <Link href="/about" className="px-4 py-2">
+        <Link href="/about" className="link">
           ABOUT
         </Link>
-        <Link href="/users" className="px-4 py-2">
+        <Link href="/users" className="link">
           USERS
         </Link>
-        <Link href="/pokemon" className="px-4 py-2">
+        <Link href="/pokemon" className="link">
           POKEMON
         </Link>
-        <Link href="/counter" className="px-4 py-2">
+        <Link href="/counter" className="link">
           COUNTER
         </Link>
-        <Link href="/deffer" className="px-4 py-2">
+        <Link href="/deffer" className="link">
           DEFFER
         </Link>
-        <Link href="/account" className="px-4 py-2">
+        <Link href="/account" className="link">
           (ACCOUNT)
         </Link>
-        <Link href="/sales" className="px-4 py-2">
+        <Link href="/sales" className="link">
           (SALES)
+        </Link>
+        <Link href="/todo" className="link">
+          TO DO
         </Link>
       </div>
     </main>
